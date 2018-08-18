340795bf21352e7b2b3394906725a0ca85ef6151
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 15:06:04 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
+require 'pry'
 def unsafe?(speed)
-
+	40 < speed && speed < 60 ? false : true
+	binding.pry
 end
 
 
 
 def not_safe?(speed)
-	
+	40 < speed && speed < 60 ? false : true
 end
-	
-
-
